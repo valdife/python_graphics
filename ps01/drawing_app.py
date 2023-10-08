305b43f5731f51b0ba548bb3ec4c6a6996e3add7
Bart <salemon876@gmail.com>
2023-10-08 17:03:52 +0200

    Created two first layers grid

--- ps01/drawing_app.py
+++ ps01/drawing_app.py
@@ -1,32 +1,44 @@
-from tkinter import Tk, RIGHT, BOTH, RAISED
-from tkinter.ttk import Frame, Button, Style
+from tkinter import *
 import json
 from models import Line, Rectangle, Point, Circle
 
 
 class DrawingApp(Frame):
-    def __init__(self):
+    def __init__(self, root: Tk) -> None:
         super().__init__()
+        self.root = root
         self.initUI()
 
-    def initUI(self):
-        self.master.title("Buttons")
-        self.style = Style()
-        self.style.theme_use("default")
+    def initUI(self) -> None:
+        # create all of the first layer containers
+        canvas_frame = Frame(self.root, bg='cyan', width=800, height=425, pady=3)
+        interface_frame = Frame(self.root, bg='red', width=800, height=275, padx=3, pady=3)
 
-        frame = Frame(self, relief=RAISED, borderwidth=1)
-        frame.pack(fill=BOTH, expand=True)
+        # layout all of the first layer containers
+        self.root.grid_rowconfigure(1, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
 
-        self.pack(fill=BOTH, expand=True)
+        canvas_frame.grid(row=0, sticky="ew")
+        interface_frame.grid(row=1, sticky="nsew")
 
-        closeButton = Button(self, text="Close")
-        closeButton.pack(side=RIGHT, padx=5, pady=5)
-        okButton = Button(self, text="OK")
-        okButton.pack(side=RIGHT)
+        # create the containers for the second layer in the interface frame
+        left_frame = Frame(interface_frame, bg='blue', width=200, height=275, pady=3)
+        center_frame = Frame(interface_frame, bg='yellow', width=400, height=275, padx=3, pady=3)
+        right_frame = Frame(interface_frame, bg='green', width=200, height=275, padx=3, pady=3)
+
+        # layout the widgets in the interface_frame
+        interface_frame.grid_rowconfigure(3, weight=1)
+        interface_frame.grid_columnconfigure(0, weight=1)
+
+        left_frame.grid(row=0, column=0, sticky="ns")
+        center_frame.grid(row=0, column=1, sticky="nsew")
+        right_frame.grid(row=0, column=2, sticky="ns")
+
+        self.root.mainloop()
 
 
 if __name__ == '__main__':
     root = Tk()
-    root.geometry('400x300+300+300')
-    app = DrawingApp()
+    root.geometry('{}x{}'.format(800, 600))
+    app = DrawingApp(root=root)
     root.mainloop()
